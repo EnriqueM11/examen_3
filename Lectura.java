12850e934004f42e5b056c84d20e3cdf05d671e6
EnriqueM11 <89398794+EnriqueM11@users.noreply.github.com>
2023-03-31 18:32:50 -0600

    Add files via upload

---
+++ Lectura.java
@@ -0,0 +1,89 @@
+package examen_3;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Lectura {
+	String archivoCSV = "/Users/sistelcomunica/eclipse-workspace/examen_3/src/examen_3/emails.csv";
+	        String linea = "";
+	        String separador = ",";
+	        int filas_a_evaluar = 2;
+	        int contador=1;
+	        int digitos;
+	        
+	   
+	
+public void ejecutar (int ID){
+	        
+	
+	digitos=ID%1000;
+	digitos=digitos-1;
+
+	        Map<String, Integer> columnas = new HashMap<>();
+
+	        try (BufferedReader br = new BufferedReader(new FileReader(archivoCSV))) {
+	            // Leer la primera línea del archivo
+	            linea = br.readLine();
+	            String[] titulos = linea.split(separador);
+
+	            // Agregar las columnas al HashMap
+	            for (int i = 1; i < titulos.length; i++) {
+	                columnas.put(titulos[i].trim(), 0);
+	            }
+
+	            // Iterar a través de las filas y sumar los valores de las columnas
+	            int cont = 0;
+	            while ((linea = br.readLine()) != null && cont < filas_a_evaluar) {
+	                String[] valores = linea.split(separador);
+	                
+
+	                if (contador == digitos) { // verificar si se ha alcanzado la fila deseada
+	                    String[] datos = linea.split(separador);
+
+	                    for (int i = 1; i < datos.length; i++) {
+	                        String columna = titulos[i];
+	                        int valor = Integer.parseInt(datos[i]);
+	                        int sumaActual = columnas.get(columna);
+	                        columnas.put(columna, sumaActual + valor);
+	                    }
+	                    cont++;
+	                }
+	                else {
+	                	contador++;
+	                }
+
+
+	           
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+
+	        // GUARDAR los resultados
+	        
+	        try {
+	            FileWriter writer = new FileWriter("174798.txt");
+	            for (Map.Entry<String, Integer> entry : columnas.entrySet()) {
+	                writer.write(entry.getKey() + ": " + entry.getValue() + "\n");
+	                
+	            }
+	            System.out.println("Documento creado con exito");
+	            writer.close();
+	        } catch (IOException e_2) {
+	            e_2.printStackTrace();
+	        }
+}
+}
+
+	        
+	        
+
+
+
+
+
---
+++ main.java
@@ -0,0 +1,12 @@
+package examen_3;
+
+public class main {
+	
+		public static void main(String args[]) {
+		Lectura miLectorExamen = new Lectura();
+		miLectorExamen.ejecutar(798);// example
+		}
+}
+
+
+
